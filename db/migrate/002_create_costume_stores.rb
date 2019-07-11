b8f324abf40aae3cd82f9b96d97254cf3529ad8d
Craig Donavin <crdonavin@gmail.com>
2019-07-11 02:10:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,11 @@
 class CreateCostumeStores
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :num_of_employees
+      t.integer :costume_inventory
+      t.string :opening_time
+      t.string :closing_time
+  end
 end
