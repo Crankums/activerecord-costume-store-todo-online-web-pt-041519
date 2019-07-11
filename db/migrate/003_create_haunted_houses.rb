087fbdfbd113a6da38e9e39cf9bff3264f7d8b46
Craig Donavin <crdonavin@gmail.com>
2019-07-11 02:21:18 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,2 +1,14 @@
 class CreateHauntedHouses
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
+    end
+  end
 end
