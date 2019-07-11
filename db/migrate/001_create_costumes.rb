5e6d263f9c7ed8c09bdcfbb0a761660fcbf82d41
Craig Donavin <crdonavin@gmail.com>
2019-07-11 02:04:04 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,4 +1,12 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.string :image_url
+      
+  end
 end
 
 # Create a class and inherit from ActiveRecord::Migration
